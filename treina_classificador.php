9a6bbeb4b062f5584086645091819c6f786115d4
sfreitascecilia <sfreitascecilia@gmail.com>
2024-02-16 03:08:25 -0300

    Inclusão de stop words

--- treina_classificador.php
+++ treina_classificador.php
@@ -21,6 +21,8 @@ use Rubix\ML\NeuralNet\ActivationFunctions\LeakyReLU;
 use Rubix\ML\NeuralNet\Optimizers\AdaMax;
 use Rubix\ML\Persisters\Filesystem;
 use Rubix\ML\Extractors\CSV;
+use Rubix\ML\Transformers\StopWordFilter;
+use Rubix\ML\Tokenizers\WordStemmer;
 
 try {
 
@@ -31,6 +33,10 @@ try {
     // Aumentando o limite de tempo para 5 minutos
 
     ini_set('max_execution_time', '300');
+
+    // Relaciona as palavras irrelevantes
+    $stopwords = ['de','a','o','que','e','do','da','em','um','para','e','com','uma','os','no','se','na','por','mais','as','dos','como','mas','foi','ao','ele','das','tem','a','seu','sua','ou','ser','quando','muito','ha','nos','ja','esta','eu','tambem','so','pelo','pela','ate','isso','ela','entre','era','depois','sem','mesmo','aos','ter','seus','quem','nas','me','esse','eles','estao','voce','tinha','foram','essa','num','nem','suas','meu','as','minha','tem','numa','pelos','elas','havia','seja','qual','sera','nos','tenho','lhe','deles','essas','esses','pelas','este','fosse','dele','tu','te','voces','vos','lhes','meus','minhas','teu','tua','teus','tuas','nosso','nossa','nossos','nossas','dela','delas','esta','estes','estas','aquele','aquela','aqueles','aquelas','isto','aquilo','estou','esta','estamos','estao','estive','esteve','estivemos','estiveram','estava','estavamos','estavam','estivera','estiveramos','esteja','estejamos','estejam','estivesse','estivessemos','estivessem','estiver','estivermos','estiverem','hei','ha','havemos','hao','houve','houvemos','houveram','houvera','houveramos','haja','hajamos','hajam','houvesse','houvessemos','houvessem','houver','houvermos','houverem','houverei','houvera','houveremos','houverao','houveria','houveriamos','houveriam','sou','somos','sao','era','eramos','eram','fui','foi','fomos','foram','fora','foramos','seja','sejamos','sejam','fosse','fossemos','fossem','for','formos','forem','serei','sera','seremos','serao','seria','seriamos','seriam','tenho','tem','temos','tem','tinha','tinhamos','tinham','tive','teve','tivemos','tiveram','tivera','tiveramos','tenha','tenhamos','tenham','tivesse','tivessemos','tivessem','tiver','tivermos','tiverem','terei','tera','teremos','terao','teria','teriamos','teriam'];
+    //var_dump($stopwords);
 
     $logger = new Screen();
 
@@ -70,7 +76,9 @@ try {
     $estimator = new PersistentModel(
         new Pipeline([
             new TextNormalizer(),
-            new WordCountVectorizer(PHP_INT_MAX, 1, 0.8, new NGram(1, 2)),
+            new StopWordFilter($stopwords),
+            new WordCountVectorizer(10000, 1, 0.8, new NGram(1, 2, new WordStemmer('portuguese'))), // MaxVocabularySize (valor antigo): PHP_INT_MAX
+            //new WordCountVectorizer(10000, 1, 0.8, new NGram(1, 2)), // MaxVocabularySize (valor antigo): PHP_INT_MAX
             new TfIdfTransformer(),
             new ZScaleStandardizer(),
         ], new MultilayerPerceptron([
